46d1ab3b3877660810f37813bc325fb4b56eca71
Raj <kmrrajj@gmial.com>
2017-12-16 22:01:57 +0530

    fixed a bug

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
 import { EMAIL_CHANGED } from './types';
 export const emailChanged = text => {
-  return (type: 'EMAIL_CHANGED'), (payload: text);
+  return {
+    type: EMAIL_CHANGED,
+    payload: text
+  };
 };
